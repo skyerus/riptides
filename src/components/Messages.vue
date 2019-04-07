b78a9b4959757abc40457a2c42f98263159b45c7
Skye <gill.skye95@gmail.com>
2019-04-07 20:26:32 +0100

    Keep scroll at bottom of messages unless user has scrolled up

---
+++ src/components/Messages.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="overflow-auto" ref="chat">
+        <template v-for="(item, index) in logs">
+            <v-subheader v-if="item" :key="index">{{ item }}</v-subheader>
+            <v-divider class="mb-2 mt-2"/>
+        </template>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Messages",
+    props: [
+      'logs',
+    ],
+    data() {
+      return {
+        scrolled: false,
+        maxScrollTop: 0
+      }
+    },
+    methods: {
+      updateScroll(scrolled) {
+        if (!scrolled) {
+          this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+        }
+        this.maxScrollTop = this.$refs.chat.scrollHeight - this.$refs.chat.clientHeight;
+      },
+
+      scroll() {
+        if (this.$refs.chat.scrollTop !== this.maxScrollTop) {
+          this.scrolled = true;
+          return;
+        }
+        this.scrolled = false;
+      }
+    },
+    watch: {
+      logs() {
+        setTimeout(() => this.updateScroll(this.scrolled), 0);
+      }
+    },
+    mounted() {
+      this.$refs.chat.addEventListener('scroll', () => this.scroll());
+    },
+    destroyed() {
+      this.$refs.chat.removeEventListener('scroll', () => this.scroll());
+    }
+  }
+</script>
+
+<style scoped>
+    .overflow-auto {
+        overflow: auto;
+    }
+</style>
--- src/pages/Tide.vue
+++ src/pages/Tide.vue
@@ -8,7 +8,6 @@
         <div class="d-inline-block row-2 pl-2 pr-2">
             <div class="flex-container flex-col-reverse height-95">
                 <div>
-                    <v-divider class="mb-3"/>
                     <div>
                         <v-text-field
                                 v-model="message"
@@ -16,12 +15,11 @@
                                 label="Message"
                                 append-icon="send"
                                 @click:append="sendMessage"
+                                @keyup.enter="sendMessage"
                         ></v-text-field>
                     </div>
                 </div>
-                <div>
-                    {{ this.msg }}
-                </div>
+                <Messages ref="messages" :logs="this.logs"/>
             </div>
         </div>
         <div class="d-inline-block vertical-align-top r-sidebar row-3 pr-2 dark-bg-0">
@@ -34,13 +32,20 @@
 
 <script>
   import handler from '../services/api/handler'
+  import Messages from '../components/Messages.vue'
 
   export default {
     name: "Tide",
+    components: {
+      Messages
+    },
     data() {
       return {
         message: '',
-        msg: '123'
+        logs: [
+          'Test message 1',
+          'Test message 2'
+        ]
       }
     },
     computed: {
@@ -52,17 +57,17 @@
     },
     methods: {
       sendMessage() {
-        console.log('send');
         this.$socket.emit('message', this.message)
       },
 
       joinTide() {
         this.$socket.emit('join', 'testing')
-      }
+      },
     },
     sockets: {
       message: function (data) {
-        this.msg = data.msg;
+        this.logs.push(data.msg)
+        this.message = ''
       },
       test: function (data) {
         console.log(`User receives: ${data.msg}`);
@@ -76,11 +81,11 @@
         if (val) {
           this.joinTide();
         }
-      }
+      },
     },
     mounted() {
       this.joinTide();
-    }
+    },
   }
 </script>
 
