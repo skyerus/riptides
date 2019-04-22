da8ef2599cbec2ee19bcdcb7014425320354adae
Skye <gill.skye95@gmail.com>
2019-04-22 16:08:47 +0100

    webpack prod config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,22 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const DotEnv = require('dotenv')
+const webpack = require('webpack')
 
 module.exports = merge(common, {
   mode: 'production',
   module: {
     rules: [
     ]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'API_URL': JSON.stringify(process.env.API_URL),
+        'CHAT_SOCKET_URL': JSON.stringify(process.env.CHAT_SOCKET_URL),
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'SPOTIFY_AUTHORIZE_URL': JSON.stringify(process.env.SPOTIFY_AUTHORIZE_URL)
+      }
+    })
+  ]
 });
