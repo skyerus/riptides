b8eb1c2596cb1dc61b6cd7b7b21e8193163970b7
Skye <gill.skye95@gmail.com>
2019-05-06 12:08:03 +0100

    Homepage

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
     <div id="app">
         <v-app id="keep" dark>
-            <Layout>
+            <div v-if="$route.fullPath === '/'">
+                <router-view :key="$route.fullPath"></router-view>
+            </div>
+            <Layout v-else>
                 <router-view :key="$route.fullPath"></router-view>
             </Layout>
         </v-app>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,30 @@
 <template>
+    <v-layout class="pt-5">
+        <v-flex xs6 md4 offset-md2>
+            <div class="xl-text">
+                Listen to music together
+            </div>
+            <h2>
 
+            </h2>
+        </v-flex>
+        <v-flex xs6 md4 offset-md2>
+
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>
+  import {store} from '../store'
+
   export default {
-    name: "Home"
+    name: "Home",
+    beforeRouteEnter(to, from, next) {
+      if (store.getters.isLoggedIn) {
+        next('/tides')
+      }
+      next()
+    }
   }
 </script>
 
