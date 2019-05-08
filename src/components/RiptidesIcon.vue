46ac11d0ac6b0c0a80d8d1580224fd6fec8aac2f
Skye Gill <gill.skye95@gmail.com>
2019-05-08 21:14:06 +0100

    Icons lnading page

--- src/components/RiptidesIcon.vue
+++ src/components/RiptidesIcon.vue
@@ -8,7 +8,6 @@
     props: {
       height: {
         type: String,
-        default: "auto"
       },
       floatLeft: {
         type: Boolean,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,28 @@
 <template>
-    <div class="white--text blue-gradient header">
-        <div class="text-xs-center ma-auto icon-height">
-            <RiptidesIcon class="vertical-align-middle" height="100%"/>
-            <span class="xl-text vertical-align-middle">riptides</span>
+    <div>
+        <div class="white--text blue-gradient header">
+            <div class="text-xs-center ma-auto icon-height">
+                <RiptidesIcon class="vertical-align-middle" height="100%"/>
+                <span class="pl-1 xl-text vertical-align-middle">riptides</span>
+            </div>
+            <div class="text-xs-center t-padding">
+                <span class="xl-text">
+                    Listen together
+                </span>
+            </div>
         </div>
-        <div class="text-xs-center t-padding">
-            <h1 class="xl-text">
-                Listen together
-            </h1>
-        </div>
+        <v-layout class="pl-4 pt-5 pr-4 text-xs-center">
+            <v-flex sm12 md4>
+                <v-icon class="large-icon-2">group</v-icon><br>
+                <span class="subheader">rgrgdgdr</span>
+            </v-flex>
+            <v-flex sm12 md4>
+                <v-icon class="large-icon-2">queue</v-icon>
+            </v-flex>
+            <v-flex sm12 md4>
+                <v-icon class="large-icon-2">compare_arrows</v-icon>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
 
@@ -40,5 +54,13 @@
     }
     .t-padding {
         padding-top: 2vh;
+    }
+    .subheader {
+        font-size: 24px;
+        line-height: 32px;
+        letter-spacing: -0.25px;
+        font-weight: 900;
+        color: #000;
+        margin: 0 0 12px;
     }
 </style>
